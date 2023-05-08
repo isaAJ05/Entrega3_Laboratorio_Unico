0498bdc445f2849d246c10cb5cf20832f9baf518
Ari <isabellajuliao@uninorte.edu.co>
2023-05-08 04:45:10 -0500

    nose ni que estoy haciendo aiuda

--- src/PanelesEscaletaYSerpiente/InfoPanel.java
+++ src/PanelesEscaletaYSerpiente/InfoPanel.java
@@ -11,53 +11,34 @@ public class InfoPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jPanel1 = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
 
         setOpaque(false);
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setFont(new java.awt.Font("Swis721 BlkEx BT", 0, 14)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(204, 153, 255));
-        jLabel1.setText("Instrucciones de Uso ");
-        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 30, -1, -1));
+        jLabel2.setFont(new java.awt.Font("Swis721 BlkEx BT", 3, 12)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 102, 51));
+        jLabel2.setText("INSTRUCCIONES DE USO ");
+        add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 30, -1, -1));
 
-        jPanel1.setBackground(new java.awt.Color(86, 67, 127));
+        jTextArea1.setEditable(false);
+        jTextArea1.setBackground(new java.awt.Color(242, 135, 63));
+        jTextArea1.setColumns(20);
+        jTextArea1.setFont(new java.awt.Font("Monospaced", 1, 13)); // NOI18N
+        jTextArea1.setRows(5);
+        jTextArea1.setText("¡Bienvenido a \"Escaleras y Serpientes versión Espacial\"! \n\nEn este juego, tendrás la oportunidad de escoger tu avatar alien y recorrer un tablero \nespacial donde competirás contra nuestra adorada Stella por el primer lugar.\n\nPor favor ten en cuenta las siguientes instrucciones: \n1. Antes de comenzar, asegúrate de elegir tu avatar. \n   Recuerda que el avatar del jugador es un alien, así que elige aquel que refleje \n   mejor tu personalidad extraterrestre.\n2. Selecciona la opción de \"Jugar\" para dirigirte a nuestro tablero espacial.\n3. ¿Listo para empezar? Lanza el dado para comenzar tu turno. \n   ¡Que la suerte del universo esté de tu lado!\n4. Si caes en una casilla con una escalera, tu avatar despegará hacia arriba. \n   ¡Prepárate para llegar más rápido a la línea final!\n5. Pero ten cuidado, si caes en una casilla con una serpiente, tu avatar espacial \n   será atrapada y descenderás de golpe. ¡Intenta evitar esas serpientes espaciales! \n7. Recuerda, debes avanzar por 35 casillas para llegar a la línea final y ganar. \n   ¡Mantén tu concentración y no pierdas de vista tu objetivo final!\n\n¡Que la fuerza alienígena te acompañe! \n¡Diviértete jugando a \"Escaleras y Serpientes versión Espacial\"!");
+        jTextArea1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jScrollPane1.setViewportView(jTextArea1);
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 346, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 321, Short.MAX_VALUE)
-        );
-
-        add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(16, 62, -1, -1));
-
-        jPanel2.setBackground(new java.awt.Color(86, 67, 127));
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 370, Short.MAX_VALUE)
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 320, Short.MAX_VALUE)
-        );
-
-        add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 60, 370, 320));
+        add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 60, 720, 330));
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
 }
