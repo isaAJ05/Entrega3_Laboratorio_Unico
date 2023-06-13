97b1f6285deb08e94b1c4dd579c0a09da1a494af
Canela <carpinteron@uninorte.edu.co>
2023-06-13 18:13:08 -0500

    cistas

--- src/PanelesCarreraNave/PanelInfo.java
+++ src/PanelesCarreraNave/PanelInfo.java
@@ -27,13 +27,14 @@ public class PanelInfo extends javax.swing.JPanel {
         add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(37, 22, -1, -1));
 
         jPanel1.setBackground(new java.awt.Color(153, 153, 255));
+        jPanel1.setOpaque(false);
 
         jTextArea1.setEditable(false);
         jTextArea1.setBackground(new java.awt.Color(153, 153, 255));
         jTextArea1.setColumns(20);
-        jTextArea1.setFont(new java.awt.Font("Monospaced", 1, 13)); // NOI18N
+        jTextArea1.setFont(new java.awt.Font("Microsoft JhengHei UI Light", 1, 13)); // NOI18N
         jTextArea1.setRows(5);
-        jTextArea1.setText("¡Bienvenido al juego de carreras espaciales! \n\nEn este juego, tendrás la oportunidad de pilotar tu propia nave espacial y competir contra\notros jugadores por el primer lugar.\n\n* Antes de empezar la carrera, podrás elegir tu nave espacial favorita. \n\n* Para iniciar una nueva partida selecciona la opción \"Carrera\" en el menú principal. \n\n* Una vez que hayas seleccionado tu nave, aparecerás en la línea de salida junto con los \n   demás pilotos.\n\n* Selecciona la opción \"Comenzar\" para jugar. Prepárate para la carrera.\n\n* Para mover tu nave, utiliza las teclas de flecha de tu teclado. \n\n                         ⬆\n                      ⬅   ➜\n                         ⬇\n\n  La tecla de flecha hacia la izquierda acelerará tu nave\n  la tecla de flecha hacia abajo frenará (LO CUAL NO ESTÁ PERMITIDO) \n  la tecla de flecha hacia arriba o abajo girará tu nave en esa dirección.\n \n* El primer jugador en cruzar la línea de meta será el ganador.\n\n* Si deseas volver a jugar, selecciona la opción \"Carrera\" en el menú principal.\n \n\n            Ahora estás listo para competir en el juego de carreras espaciales. \n                                \n                                 ¡Que gane el mejor piloto!");
+        jTextArea1.setText("            \n            ¡Bienvenido al juego de carreras espaciales! \n\n En este juego, tendrás la oportunidad de pilotar tu propia\n nave espacial y competir contra otros jugadores por el\n primer lugar.\n\n     *  Antes de empezar la carrera, podrás elegir tu nave\n        espacial favorita. \n\n     *  Para iniciar una nueva partida selecciona la opción \n        \"Carrera\" en el menú principal. Si deseas hacer uso \n        del control remoto, puede seleccionar la opción de \n        \"Remoto\". \n\n     *  Una vez que hayas seleccionado tu nave, aparecerás\n        en la línea de salida junto con los demás pilotos.\n\n     *  Selecciona la opción \"Comenzar\" para jugar. \n         Prepárate para la carrera.\n\n     *  Para mover tu nave, utiliza las teclas de flecha de tu \n        teclado. \n\n             ↑              La tecla de flecha hacia arriba o abajo\n        ←       →         dirijira tu nave en esa dirección.\n             ↓              La tecla de flecha hacia la derecha\n                              acelerará tu nave\n \n      * Aprovecha los bonos de la pista !\n        Las esferraz de luz de color azul te ayudaran a \n        acelarar el motor, asi iras mas rápido.\n \n       * Esquiva los obstaculos de la pista !\n        Las esferaz de luz de color rojo harán que retrocedas\n        en tu camino. Ten ciudado !\n      \n       * El primer jugador en cruzar la línea de meta será\n          el ganador.\n\n       *  Si deseas volver a jugar, selecciona la opción \n         \" Carrera\" en el menú principal.\n \n\n      Ahora estás listo para competir en el juego de \n                             carreras espaciales. \n                                \n                      ¡Que gane el mejor piloto!\n\n");
         jTextArea1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jScrollPane1.setViewportView(jTextArea1);
 
@@ -42,17 +43,17 @@ public class PanelInfo extends javax.swing.JPanel {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 750, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 407, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 284, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 304, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
-        add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 50, 750, 290));
+        add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 50, 410, 310));
     }// </editor-fold>//GEN-END:initComponents
 
 
